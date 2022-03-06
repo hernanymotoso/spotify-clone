4a7871cc6c9b25f52c358413223e46863ddb1371
Hernany Motoso <contatohernany@gmail.com>
2022-03-06 20:53:35 -0300

    Implemented NextAuth Authentication and Spotify Web API

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  console.log('Login');
+
+  return (
+    <div>
+      <h1>This is a login page</h1>
+    </div>
+  );
+};
+
+export default Login;
