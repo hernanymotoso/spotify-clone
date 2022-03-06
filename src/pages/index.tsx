91049275c7a96501e1317890d54acc7a4f319018
Hernany Motoso <contatohernany@gmail.com>
2022-03-06 17:38:28 -0300

    Initial-setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home: React.FC = () => (
+  <div className="">
+    <h1>code</h1>
+
+    <main>
+      {/* Sidebar */}
+      {/* center */}
+    </main>
+
+    <div> {/* player */} </div>
+  </div>
+);
+
+export default Home;
